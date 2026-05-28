f399a3e201327bce831a8f80a8b6d4307c3d10dd
honeykimmm <50395635+honeykimmm@users.noreply.github.com>
2026-05-28 16:06:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,37 @@
-const CACHE = 'verb-master-v2';
-const FILES = [
-  './index.html',
-  './icon-192.png',
-  './icon-512.png',
-  './icon-180.png',
-  './manifest.json'
-];
+const CACHE_NAME = 'verb-master-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png',
+        './icon-180.png',
+      ]).catch(() => {
+        // 일부 파일 없어도 설치 계속
+        return cache.add('./index.html');
+      });
+    })
+  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
+  );
 });
